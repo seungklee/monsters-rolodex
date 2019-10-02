93024361a1f4e9d727823780246bc996e18b2c10
seungklee <steve.lee8580@gmail.com>
2019-10-01 18:48:40 -0700

    Updates

---
+++ precache-manifest.b17a844524fbf4ca266ed7dcb24c0033.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "050b790eaa54ee12486c458508b71888",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6d798a9a25623b5303a3",
+    "url": "/monsters-rolodex/static/css/main.76551c0d.chunk.css"
+  },
+  {
+    "revision": "877ab33c4cabc00823c7",
+    "url": "/monsters-rolodex/static/js/2.4586650f.chunk.js"
+  },
+  {
+    "revision": "6d798a9a25623b5303a3",
+    "url": "/monsters-rolodex/static/js/main.d8845fe5.chunk.js"
+  },
+  {
+    "revision": "29b8639263678424eef0",
+    "url": "/monsters-rolodex/static/js/runtime-main.7352cc78.js"
+  }
+]);
